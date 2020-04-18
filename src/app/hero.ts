d3a4a8af60452f8281ec1e9bcb47b2f9363e4c2c
ValimGustavo <gustavo.souza.valim@gmail.com>
2020-04-18 19:00:30 -0300

    2 files modified

--- src/app/hero.ts
+++ src/app/hero.ts
@@ -2,8 +2,8 @@ export class Hero {
  constructor(
     public id: number;
     public name: string;
-    public alterEgo?: string; //opcional
     public power: string;
+    public alterEgo?: string; //opcional
  ){}
   
 }
